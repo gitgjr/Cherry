b717786d15924e1c1b4d73d475fdbbf0933599cc
Geng JIERAN <entheos_work@outlook.com>
2024-01-22 11:28:56 +0900

    Linux test2

---
+++ docker/kubo0.25/mount/projectInit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -ex
+apt install sudo
+sudo apt upgrade
+sudo apt -y install ffmpeg
+cd /home
+git clone https://github.com/gitgjr/Cherry.git
+mkdir Cherry/data
+mkdir Cherry/data/resource
+mkdir Cherry/data/serverWork
+mkdir Cherry/data/p2pWork
+
+cp /resource/* /home/Cherry/data/resource
